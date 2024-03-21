7aca0fe8e1bfb5758746a27bbbb3ccad08f3e049
혜빈 문 <munhyebin40@gmail.com>
2024-03-21 16:11:02 +0900

    urls, views, templates playground/hello_html/ -> say_hello_html() -> hello.html

--- playground/views.py
+++ playground/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def say_hello(request):
     return HttpResponse("Hllo World")
+
+def say_hello_html(request):
+    return render(request, 'playground/hello.html')
