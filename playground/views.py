d0d127bd859305f52c1c3f26cdbf2b77b6a43955
혜빈 문 <munhyebin40@gmail.com>
2024-03-25 12:16:56 +0900

    views -> templates에 context 전달

--- playground/views.py
+++ playground/views.py
@@ -6,7 +6,11 @@ def say_hello(request):
     return HttpResponse("Hllo World")
 
 def say_hello_html(request):
-    return render(request, 'playground/hello.html')
+    return render(request, 'playground/hello.html', {'name' : '혜빈아', 'greeting': '안녀엉~'})
 
 def say_bye_html(request):
-    return render(request, 'playground/bye.html')
+    context = {
+        'singer' : '부승관',
+        'title' : '동그라미',
+    }
+    return render(request, 'playground/bye.html', context = context)
